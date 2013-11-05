80f6f61dfad3b6e45f8ef5db3071bae4e58c691f
Michael Hagedorn <michael.hagedorn@live.de>
2013-11-05 20:32:35 +0100

    c_input.php als Userinput-Klasse definiert

---
+++ classes/c_input.php
@@ -0,0 +1,19 @@
+<?php
+
+// Diese Klasse soll dafür verwendet werden sämtlichen Userinput zu 
+// filtern (mit Standardfilterung sowie Bereinigung
+
+class c_input 
+{
+
+    function __construct() {
+        
+    }
+    
+    function filter()
+    {
+        
+    }
+
+}
+?>
